322a08a42741764d0a1091b073fd696c81ba7883
GeronimoOlanda <geronimo.olanda@outlook.com>
2021-09-06 00:24:59 -0300

    Adicionando shadow no card

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import './index.css';
 import P from 'prop-types';
 
 export const Card = ({ value }) => {
   return (
-    <div className="App">
+    <div className="Card">
       <p key={value.id}>{value.joke}</p>
     </div>
   );
